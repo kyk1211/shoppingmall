bdcaf5919fa43a0af4cc203cfc414f78f016016e
곽윤구 <dbsrndi123@naver.com>
2021-10-13 20:47:41 +0900

    useEffect

--- src/Detail.js
+++ src/Detail.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router';
 import styled from 'styled-components';
 import './Detail.scss';
@@ -14,7 +14,18 @@ const Title = styled.h4`
 `;
 
 export default function Detail({ goods }) {
-
+  const [alert, setAlert] = useState(true);
+  const [inputValue, setInputValue] = useState('');
+  
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setAlert(false);
+    }, 2000)
+    return () => {
+      clearTimeout(timer);
+    }
+  }, []);
+  
   const { id } = useParams();
   const history = useHistory();
   const findGoods = goods.find(item => item.id == id)
@@ -23,16 +34,18 @@ export default function Detail({ goods }) {
   return (
     <div className="container">
       <Box>
-        <Title className="red">Detail</Title>
+        <Title className="black">Detail</Title>
         {/* <Title color="red">Detail</Title> */}
       </Box>
+      { inputValue }
+      <input onChange={(e) => {setInputValue(e.target.value)}}/>
 
-      <div className="my-alert">
+      {/* <div className="my-alert">
         <p>sold out</p>
-      </div>
-      <div className="my-alert2">
+      </div> */}
+      {alert ? (<div className="my-alert2">
         <p>sold out</p>
-      </div>
+      </div>) : null}
 
       <div className="row">
         <div className="col-md-6">
